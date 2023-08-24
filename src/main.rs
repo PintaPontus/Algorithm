cdf75af53fa317bbd87da47b17062444a2de4730
Blasco 001 <matteo.blascoviz@gmail.com>
2023-08-24 14:52:35 +0200

    simple afk script

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use mouse_rs::{Mouse};
 use std::{thread, time};
 
-
-
 fn main() {
     let mouse = Mouse::new();
     loop {
-        mouse.move_to(500, 500).expect("Unable to move mouse");
-        thread::sleep(time::Duration::from_millis(10000));
-        mouse.move_to(510, 510).expect("Unable to move mouse");
-        thread::sleep(time::Duration::from_millis(10000));
+        let position = mouse.get_position().expect("Unable to retrieve mouse position");
+        thread::sleep(time::Duration::from_millis(5000));
+        mouse.move_to(position.x+100, position.y).expect("Unable to move mouse");
+        thread::sleep(time::Duration::from_millis(5000));
+        mouse.move_to(position.x, position.y).expect("Unable to move mouse");
     }
 }
