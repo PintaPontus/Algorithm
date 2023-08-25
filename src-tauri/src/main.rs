66390296c57198604c177b6e253e0efe82f68102
Blasco 001 <matteo.blascoviz@gmail.com>
2023-08-25 14:44:42 +0200

    finalization

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,20 +7,9 @@ use mouse_rs::types::keys::Keys;
 use mouse_rs::types::Point;
 use serde::ser::SerializeStruct;
 use serde::{Serializer};
-use tauri::Manager;
 
 fn main() {
   tauri::Builder::default()
-      // TODO: remove call to setup to remove devtools
-      .setup(|app| {
-      #[cfg(debug_assertions)] // only include this code on debug builds
-      {
-        let window = app.get_window("main").unwrap();
-        window.open_devtools();
-        window.close_devtools();
-      }
-      Ok(())
-    })
     .invoke_handler(tauri::generate_handler![logging, click, move_mouse, get_coords])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
